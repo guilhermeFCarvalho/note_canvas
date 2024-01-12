b6027892b17cd5948d059186485d86464bd724ca
Guilherme <gui.carvalho021@gmail.com>
2024-01-12 16:53:27 -0300

    feat:  add router

---
+++ lib/app/core/routes/routes.dart
@@ -0,0 +1,16 @@
+import 'package:g_notes/app/presentation/notes_list/pages/notes_list.dart';
+import 'package:go_router/go_router.dart';
+
+final routes = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (_, __) => const NotesListPage(),
+    ),
+    GoRoute(
+      path: '/new-note',
+      // builder: (_, __) => const NotesListPage(),
+    ),
+
+  ],
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:g_notes/app/presentation/list_notes/pages/list_notes_page.dart';
+import 'package:g_notes/app/presentation/notes_list/pages/notes_list.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ListNotesPage(),
+      home: NotesListPage(),
     );
   }
 }
